5ace5f0858541a39f3aec55b051d3008827eba3f
GabrielMaya <74007550+GabrielMaya@users.noreply.github.com>
2022-03-04 21:46:43 -0300

    Incluido funções e tala de autenticação

--- core/views.py
+++ core/views.py
@@ -1,15 +1,41 @@
 from django.shortcuts import render, redirect
 from core.models import Evento
-# Create your views here.
+from django.contrib.auth.decorators import login_required  # para só acessar uma página se estiver logado
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 # def index(request):
 #    return redirect('/agenda/')   # segunda opção redirecionamento de diretorio em branco na url
 
+def login_user(request):
+    return render(request, 'login.html')
 
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request, "Usuário ou senha inválido")
+
+    return redirect('/')
+
+
+@login_required(login_url='/login/')
 def lista_eventos(request):
-    # usuario = request.user
-    evento = Evento.objects.all()  # mostra todos os eventos cadastrados
-    # evento = Evento.objects.filter(usuario=usuario)  # só vai retonar os eventos de cada usuário
-    data = {'eventos' :evento}
+    usuario = request.user
+    user = request.user
+    # evento = Evento.objects.all()  # mostra todos os eventos cadastrados
+    evento = Evento.objects.filter(usuario=usuario)  # só vai retonar os eventos de cada usuário
+    data = {'eventos': evento}
     return render(request, 'agenda.html', data)
